cb8480f331e535df8eeae0da99601fb95cedfed7
Harounbacha <151841258+Harounbacha@users.noreply.github.com>
2025-05-25 12:14:27 +0200

    Initial commit: Add image processing script

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+import cv2
+# read image 
+image_path = os.path.join('.','data','image.png')
+
+img = cv2.imread(image_path)
+
+# write image 
+write_path = os.path.join('.','data','image_out.png')
+cv2.imwrite(write_path, img)
+# show image or visualize
+cv2.imshow('frame', img)
+cv2.waitKey(0) 
+
+
