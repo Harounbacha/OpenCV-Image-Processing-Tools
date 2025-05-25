8649206e5d57467994dff6ad5d32581cf006e681
Harounbacha <151841258+Harounbacha@users.noreply.github.com>
2025-05-25 18:22:58 +0200

    adding edges dit

--- Threshold.py
+++ Threshold.py
@@ -5,9 +5,10 @@ import cv2
 img = cv2.imread(os.path.join('.','data','scarface.jpg')) # read the image
 print(img.shape)
 image_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # convert to gray scale
-ret ,threshold = cv2.threshold(image_gray,50 ,255,cv2.THRESH_BINARY) # apply thresholding
-threshold = cv2.resize(threshold, (600,800)) 
-img = cv2.resize(img, (600,800)) #
+ret ,threshold = cv2.threshold(image_gray,40 ,255,cv2.THRESH_BINARY) # apply thresholding
+cv2.medianBlur(threshold, 5, threshold)
+threshold = cv2.resize(threshold, (600,800))
+img = cv2.resize(img, (600,800))
 cv2.imshow('original', img)
 cv2.imshow('threshold', threshold) # show the thresholded image
 cv2.waitKey(0)
